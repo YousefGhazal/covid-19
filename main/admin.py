4a1cf55a65ef353b1ed814e758614687e44a63aa
Yousef <yousefghazal2412@gmail.com>
2022-12-21 01:42:59 +0200

    create model

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from solo.admin import SingletonModelAdmin
-from .config import SiteConfiguration
 from .models import Section
 # Register your models here.
-admin.site.register(SiteConfiguration, SingletonModelAdmin)
-@admin.register(Section)
-class SectionAdmin(admin.ModelAdmin):
-    list_editable = ['published']
-    list_display = ['title', 'published']
-    search_fields = ['country']
+admin.site.register(Section, SingletonModelAdmin)
--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from solo.models import SingletonModel
 from ordered_model.models import OrderedModel
 
 # Create your models here.
-class Section(OrderedModel):
+class Section(OrderedModel, SingletonModel):
     title = models.CharField(max_length=50, blank=False)
     subheading = models.TextField(blank=True)
     link = models.URLField(max_length=200, blank=True)
